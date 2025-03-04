717ba1c4b30e0ab7a4cac49b11c03f0a8a56d759
ruonannn <xionrx@student.unimelb.edu.au>
2025-03-04 15:32:12 +1100

    add tests for 'Subtraction of 2 integers' and  'Division of 2 integers'

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,12 +3,13 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
+
         return 4;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
@@ -18,6 +19,9 @@ public class Calculator {
 
     public double divide(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
     }
 }
